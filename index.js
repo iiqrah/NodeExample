c517d04ce66facb5ac33c823e504c31817bc5e65
SamWhale <samwhale1999@gmail.com>
2018-06-16 18:08:32 +0100

    Adding path import

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const path = require('path')
 
 app
 .use(express.static(path.join(__dirname, 'public')))
