fd131cd5bdc5cce669396f36cb46133b89d4e584
SamWhale <samwhale1999@gmail.com>
2018-06-16 18:04:22 +0100

    Changes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('App running on port 3000')
+app.listen(PORT, function () {
+  console.log('App unning on port ' + PORT)
 })
