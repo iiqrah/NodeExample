28e5f82cecff27e9e5042e1b6056cdacffb6cc70
SamWhale <samwhale1999@gmail.com>
2018-06-16 18:07:19 +0100

    Adding heroku template page

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 const express = require('express')
 const app = express()
-
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
+app
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('pages/index');
 })
 
 app.listen(PORT, function () {
