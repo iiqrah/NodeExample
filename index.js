f0f87c6c724c2f14cb63435294f3f75b4ddfc2af
SamWhale <samwhale1999@gmail.com>
2018-06-17 19:41:17 +0100

    Making sure index.js is here

---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+const path = require('path')
+const bodyParser = require('body-parser')
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db/dev.db');
+
+app
+.use(express.static(path.join(__dirname, 'public')))
+.use(bodyParser.urlencoded({ extended: false }))
+.use(bodyParser.json())
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('pages/index');
+})
+
+app.post('/weapon_search', function (req,res) {
+  var statement = ""
+  var results = []
+
+  if (req.body.gun === "any") {
+    statement = 
+    'SELECT ' +
+      'PrimaryWeapons.WeaponName AS PrimaryName, ' + 
+      'SecondaryWeapons.WeaponName AS SecondaryName, ' +
+      'MeleeWeapons.WeaponName AS MeleeName, ' +
+      'Cards.Slot1, Cards.Slot2, Cards.Slot3, Cards.Generation ' +
+    'FROM Cards ' +
+      'INNER JOIN PrimaryWeapons ON Cards.Slot1 = PrimaryWeapons.WeaponID ' +
+      'INNER JOIN SecondaryWeapons ON Cards.Slot2 = SecondaryWeapons.WeaponID ' +
+      'INNER JOIN MeleeWeapons ON Cards.Slot3 = MeleeWeapons.WeaponID' +
+    ';'
+  } else {
+    statement = 
+    'SELECT ' +
+      'PrimaryWeapons.WeaponName AS PrimaryName, ' + 
+      'SecondaryWeapons.WeaponName AS SecondaryName, ' +
+      'MeleeWeapons.WeaponName AS MeleeName, ' +
+      'Cards.Slot1, Cards.Slot2, Cards.Slot3, Cards.Generation ' +
+    'FROM Cards ' +
+      'INNER JOIN PrimaryWeapons ON Cards.Slot1 = PrimaryWeapons.WeaponID ' +
+      'INNER JOIN SecondaryWeapons ON Cards.Slot2 = SecondaryWeapons.WeaponID ' +
+      'INNER JOIN MeleeWeapons ON Cards.Slot3 = MeleeWeapons.WeaponID ' +
+    'WHERE Slot1 == \"' + req.body.gun + "\""
+    ';'
+  }
+
+  console.log("Statement: " + statement)
+
+  db.each(statement, function (err, row) {
+    if (err) {
+      console.log("Error: " + err)
+    } else {
+      console.log(row)
+      results.push(row)
+    }
+  }, function (err, row) {
+    console.log("Complete. Results: " + results)
+    res.render('pages/index', {data: results});
+  })
+   
+})
+
+app.listen(PORT, function () {
+  console.log('App running on port ' + PORT)
+})
