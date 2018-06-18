b436e1ddf6b7af6dbfe4b09d038b8321ed066e4c
SamWhale <samwhale1999@gmail.com>
2018-06-18 19:24:08 +0100

    Prepare statement working

--- index.js
+++ index.js
@@ -16,7 +16,34 @@ app
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs');
 
-function prepareStatement(){}
+function prepareStatement(options){
+  var statement = 
+'SELECT ' +
+  'PrimaryWeapons.WeaponName AS PrimaryName, ' + 
+  'SecondaryWeapons.WeaponName AS SecondaryName, ' +
+  'MeleeWeapons.WeaponName AS MeleeName, ' +
+  'Cards.Slot1, Cards.Slot2, Cards.Slot3, Cards.Generation, Mercs.Name AS Merc, ' +
+  'Augments1.Name as Augment1, Augments2.Name as Augment2, Augments3.Name as Augment3 ' +
+'FROM Cards ' +
+  'INNER JOIN PrimaryWeapons ON Cards.Slot1 = PrimaryWeapons.WeaponID ' +
+  'INNER JOIN SecondaryWeapons ON Cards.Slot2 = SecondaryWeapons.WeaponID ' +
+  'INNER JOIN MeleeWeapons ON Cards.Slot3 = MeleeWeapons.WeaponID ' +
+  'INNER JOIN Mercs ON Cards.MercID = Mercs.MercID ' +
+  'INNER JOIN Augments1 ON Cards.Augment1 = Augments1.AugmentID ' +
+  'INNER JOIN Augments2 ON Cards.Augment2 = Augments2.AugmentID ' +
+  'INNER JOIN Augments3 ON Cards.Augment3 = Augments3.AugmentID '
+
+  if (options) {
+    statement = statement + 'WHERE ';
+    if (options.gun) {
+      statement = statement + 'Slot1 == \"' + options.gun + "\" AND "
+    }
+  }
+
+  statement = statement.slice(0,-5)
+  statement = statement + ';'
+  return statement;
+}
 
 app.get('/', function (req, res) {
   res.render('pages/index');
@@ -27,40 +54,9 @@ app.post('/weapon_search', function (req,res) {
   var results = []
 
   if (req.body.gun === "any") {
-    statement = 
-    'SELECT ' +
-      'PrimaryWeapons.WeaponName AS PrimaryName, ' + 
-      'SecondaryWeapons.WeaponName AS SecondaryName, ' +
-      'MeleeWeapons.WeaponName AS MeleeName, ' +
-      'Cards.Slot1, Cards.Slot2, Cards.Slot3, Cards.Generation, Mercs.Name AS Merc, ' +
-      'Augments1.Name as Augment1, Augments2.Name as Augment2, Augments3.Name as Augment3 ' +
-    'FROM Cards ' +
-      'INNER JOIN PrimaryWeapons ON Cards.Slot1 = PrimaryWeapons.WeaponID ' +
-      'INNER JOIN SecondaryWeapons ON Cards.Slot2 = SecondaryWeapons.WeaponID ' +
-      'INNER JOIN MeleeWeapons ON Cards.Slot3 = MeleeWeapons.WeaponID ' +
-      'INNER JOIN Mercs ON Cards.MercID = Mercs.MercID ' +
-      'INNER JOIN Augments1 ON Cards.Augment1 = Augments1.AugmentID ' +
-      'INNER JOIN Augments2 ON Cards.Augment2 = Augments2.AugmentID ' +
-      'INNER JOIN Augments3 ON Cards.Augment3 = Augments3.AugmentID ' +
-    ';'
+    statement = prepareStatement();
   } else {
-    statement = 
-    'SELECT ' +
-      'PrimaryWeapons.WeaponName AS PrimaryName, ' + 
-      'SecondaryWeapons.WeaponName AS SecondaryName, ' +
-      'MeleeWeapons.WeaponName AS MeleeName, ' +
-      'Cards.Slot1, Cards.Slot2, Cards.Slot3, Cards.Generation, Mercs.Name AS Merc, ' +
-      'Augments1.Name as Augment1, Augments2.Name as Augment2, Augments3.Name as Augment3 ' +
-    'FROM Cards ' +
-      'INNER JOIN PrimaryWeapons ON Cards.Slot1 = PrimaryWeapons.WeaponID ' +
-      'INNER JOIN SecondaryWeapons ON Cards.Slot2 = SecondaryWeapons.WeaponID ' +
-      'INNER JOIN MeleeWeapons ON Cards.Slot3 = MeleeWeapons.WeaponID ' +
-      'INNER JOIN Mercs ON Cards.MercID = Mercs.MercID ' +
-      'INNER JOIN Augments1 ON Cards.Augment1 = Augments1.AugmentID ' +
-      'INNER JOIN Augments2 ON Cards.Augment2 = Augments2.AugmentID ' +
-      'INNER JOIN Augments3 ON Cards.Augment3 = Augments3.AugmentID ' +
-    'WHERE Slot1 == \"' + req.body.gun + "\""
-    ';'
+    statement = prepareStatement({gun: req.body.gun});
   }
 
   console.log("Statement: " + statement)
